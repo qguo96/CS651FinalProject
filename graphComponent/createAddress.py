932854193ae7244f1c22198057a715931ec66af5
Qing Guo <q35guo@uwaterloo.ca>
2021-12-09 03:39:20 -0500

    add code

--- graphComponent/createAddress.py
+++ graphComponent/createAddress.py
@@ -4,7 +4,10 @@ from pyspark.sql.types import *
 from graphframes import *
 from pyspark.sql import SQLContext
 from pyspark import SparkContext,SparkConf
+import pandas as pd
 
+# Commend to run this
+# spark-submit --master local[*] --packages graphframes:graphframes:0.8.2-spark3.0-s_2.12
 spark = SparkSession \
     .builder \
     .appName("Python Spark SQL basic example") \
@@ -14,7 +17,8 @@ spark = SparkSession \
 sc = spark.sparkContext
 #sqlContext = SparkConf().setAppName("the apache sparksql")
 
-address_path = "addrs.csv"
+address_path = "addrs copy.csv"
+#address_path = "addrs.csv"
 
 #payer_address, payee_address, value, tx_hash, time
 address_schema = StructType() \
@@ -32,23 +36,35 @@ df_address = spark.read.format("csv") \
 
 #df_address.show()
 # address with day
-df2 = df_address.withColumn("time", df_address["time"].substr(1, 10))
+df2 = df_address.withColumn("time", df_address["time"].substr(1, 13))
+df3 = df2.withColumn("time", F.regexp_replace('time', 'T', '-'))
+
 
 # Creates a temporary view using the DataFrame
-df2.createOrReplaceTempView("address")
+df3.createOrReplaceTempView("address")
 
+unique_time = spark.sql("select distinct time from address")
+unique_time.show()
+time_list = unique_time.select(F.collect_set('time').alias('time')).first()['time'] 
 
+time_degree_dict = {i:0 for i in time_list}
+
+print(time_degree_dict)
 # SQL can be run over DataFrames that have been registered as a table.
 nodes = spark.sql("(select payer_address as id from address) union (select payee_address as id from address)")
 
-#nodes.show()
-#nodes_payer = spark.sql("select payer_address as src, payee_address as dst, time as relationship from address")
 
+#nodes.show()
 edges = spark.sql("select payer_address as src, payee_address as dst, time as relationship from address")
 
 g_all = GraphFrame(nodes, edges)
 
+for each_time_frame in time_list:
+      edge_condition = "relationship = '" + each_time_frame + "'"
+      #print(edge_condition)
+      mean_degree_each_time = g_all.filterEdges(edge_condition).dropIsolatedVertices().degrees.select(F.avg("degree")).collect()[0][0]
+      time_degree_dict[each_time_frame] = mean_degree_each_time
 
-inDegree = g_all.inDegrees
+df = pd.DataFrame(time_degree_dict.items(), columns=['Date', 'mean_degree'])
 
-degrees = g_all.degrees.show()
+df.to_csv("mean_degree.csv", index=False)
--- graphComponent/createTransaction.py
+++ graphComponent/createTransaction.py
@@ -22,15 +22,21 @@ transaction_schema = StructType() \
       .add("num_outputs",IntegerType(),True)
       
 df_transaction = spark.read.format("csv") \
-        .option("delimiter", ",") \
+      .option("delimiter", ",") \
       .option("header", True) \
       .schema(transaction_schema) \
       .load(transaction_path)
 
 #df_transaction.show()
 
-df2= df_transaction.withColumn("timestamp", df_transaction["timestamp"].substr(1, 10))
+df2 = df_transaction.withColumn("timestamp", df_transaction["timestamp"].substr(1, 13))
 df2.show()
+df3 = df2.withColumn("timestamp", F.regexp_replace('timestamp', 'T', '-'))
 
+#df3.show()
+# Creates a temporary view using the DataFrame
+df3.createOrReplaceTempView("transaction")
 
-df2.count()
+results = spark.sql("select timestamp as Date, count(tx_hash) as num_tx, mean(tx_value) as mean_tx_value from transaction group by timestamp")
+
+results.write.option("header", "true").mode("overwrite").csv("transaction_info.csv")
